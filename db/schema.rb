3fdec6cfa4a238376c9bd5b91f330fc6a5cf205b
JimCMorrison <Jim@CodingMorrison.com>
2017-10-26 08:54:13 -0700

    Scaffolded Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026081310) do
+ActiveRecord::Schema.define(version: 20171026155109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20171026081310) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.index ["type"], name: "index_ckeditor_assets_on_type", using: :btree
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.text     "content",    null: false
+    t.text     "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
