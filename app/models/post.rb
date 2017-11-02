8d7f37ed06bb8bf20eaad2c7e8496df047c587f3
JimCMorrison <Jim@CodingMorrison.com>
2017-11-02 05:38:18 -0700

    Added Post Model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   has_attached_file :title_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :title_image, content_type: /\Aimage\/.*\z/
+  validates :title, presence: true
+  validates :content, presence: true, length: { minimum: 15 }
 end
