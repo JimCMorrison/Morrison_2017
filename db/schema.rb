9bb61c14fcd20344905a7edffc04feda587186e6
JimCMorrison <Jim@CodingMorrison.com>
2017-11-01 22:02:07 -0700

    Setup Posts to handle Paperclip attachments labeled title_image and altered to Blog Index view to showcase it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026155109) do
+ActiveRecord::Schema.define(version: 20171102033246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,11 +29,15 @@ ActiveRecord::Schema.define(version: 20171026155109) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.text     "content",    null: false
+    t.string   "title",                    null: false
+    t.text     "content",                  null: false
     t.text     "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "title_image_file_name"
+    t.string   "title_image_content_type"
+    t.integer  "title_image_file_size"
+    t.datetime "title_image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
